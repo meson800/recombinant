21073e64b3196a48fe8924c4a806e429f3127d19
Christopher Johnstone <meson800@gmail.com>
2019-10-20 19:55:34 -0400

    Initial commit

---
+++ recombinant/recombinant.cpp
@@ -0,0 +1,12 @@
+﻿// recombinant.cpp : Defines the entry point for the application.
+//
+
+#include "recombinant.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
