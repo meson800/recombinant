dd956433da3935028a44263f389ae855cbd53785
Christopher Johnstone <meson800@gmail.com>
2019-10-20 23:24:49 -0400

    Added clang format, and removed ugly tabs

--- recombinant/main.cpp
+++ recombinant/main.cpp
@@ -1,17 +1,17 @@
-﻿//	Copyright(C) 2019 Christopher Johnstone
+﻿//  Copyright(C) 2019 Christopher Johnstone
 //
-//	This program is free software; you can redistribute itand /or modify
-//	it under the terms of the GNU General Public License as published by
-//	the Free Software Foundation; either version 2 of the License, or
-//	(at your option) any later version.
+//  This program is free software; you can redistribute itand /or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
 //
-//	This program is distributed in the hope that it will be useful,
-//	but WITHOUT ANY WARRANTY; without even the implied warranty of
-//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-//	GNU General Public License for more details.
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
 //
-//	You should have received a copy of the GNU General Public License along
-//	with this program; if not, write to the Free Software Foundation, Inc.,
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write to the Free Software Foundation, Inc.,
 //	51 Franklin Street, Fifth Floor, Boston, MA 02110 - 1301 USA.
 //
 //  Contact information:
@@ -20,6 +20,6 @@
 
 int main()
 {
-	std::cout << "Hello CMake.\n" ;
-	return 0;
+    std::cout << "Hello CMake.\n";
+    return 0;
 }
