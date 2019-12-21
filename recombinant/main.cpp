f425e09e583ad12cd97488981416b9eee8af5b53
Christopher Johnstone <meson800@gmail.com>
2019-12-20 23:16:21 -0500

    Simplified code to hello world example

--- recombinant/main.cpp
+++ recombinant/main.cpp
@@ -13,7 +13,66 @@
 //  You should have received a copy of the GNU General Public License along
 //  with this program; if not, write to the Free Software Foundation, Inc.,
 //	51 Franklin Street, Fifth Floor, Boston, MA 02110 - 1301 USA.
-#include <iostream>
-#include "MainApp.h"
 // Use wxWidget's macro to create WinMain, or the equivilant on MacOSX/Linux...
-wxIMPLEMENT_APP(MainInterface);
+// wxWidgets "Hello World" Program
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx_headers.h"
+class MyApp : public wxApp
+{
+   public:
+    virtual bool OnInit();
+};
+class MyFrame : public wxFrame
+{
+   public:
+    MyFrame();
+
+   private:
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+};
+enum
+{
+    ID_Hello = 1
+};
+wxIMPLEMENT_APP(MyApp);
+bool MyApp::OnInit()
+{
+    MyFrame* frame = new MyFrame();
+    frame->Show(true);
+    return true;
+}
+MyFrame::MyFrame()
+    : wxFrame(NULL, wxID_ANY, "Hello World")
+{
+    wxMenu* menuFile = new wxMenu;
+    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
+        "Help string shown in status bar for this menu item");
+    menuFile->AppendSeparator();
+    menuFile->Append(wxID_EXIT);
+    wxMenu* menuHelp = new wxMenu;
+    menuHelp->Append(wxID_ABOUT);
+    wxMenuBar* menuBar = new wxMenuBar;
+    menuBar->Append(menuFile, "&File");
+    menuBar->Append(menuHelp, "&Help");
+    SetMenuBar(menuBar);
+    CreateStatusBar();
+    SetStatusText("Welcome to wxWidgets!");
+    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
+    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
+    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+}
+void MyFrame::OnExit(wxCommandEvent&)
+{
+    Close(true);
+}
+void MyFrame::OnAbout(wxCommandEvent&)
+{
+    wxMessageBox("This is a wxWidgets Hello World example", "About Hello World",
+        wxOK | wxICON_INFORMATION);
+}
+void MyFrame::OnHello(wxCommandEvent&)
+{
+    wxLogMessage("Hello world from wxWidgets!");
+}
