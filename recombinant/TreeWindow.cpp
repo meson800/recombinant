fd013b40c8510242a71f7c7fc3b2334e0d60cb2a
Christopher Johnstone <meson800@gmail.com>
2020-01-02 23:18:41 -0500

    Fixed bug from last commit, didn't delete all treelist references

--- recombinant/TreeWindow.cpp
+++ recombinant/TreeWindow.cpp
@@ -8,7 +8,6 @@ TreeWindow::TreeWindow(wxWindow* parent, wxWindowID id)
     loadDataControl = new wxStaticText(
         this, wxID_ANY, localizedString(StringID::AddDataSources));
     sizer = new wxGridSizer(1, 1, 10, 10);
-    treelist->Hide();
     sizer->Add(loadDataControl, 0, wxALIGN_CENTER);
     SetSizerAndFit(sizer);
 }
