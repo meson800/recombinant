79981f6ec92771adb721172ec4889d4242a6f04d
Christopher Johnstone <meson800@gmail.com>
2020-01-07 20:04:16 -0500

    Added Windows icon support

--- recombinant/MainFrame.cpp
+++ recombinant/MainFrame.cpp
@@ -9,8 +9,13 @@
 #include "strings.h"
 
 MainFrame::MainFrame()
-    : wxFrame(NULL, wxID_ANY, localizedString(StringID::ProgramName), wxDefaultPosition, wxSize(500, 600))
+    : wxFrame(NULL, wxID_ANY, localizedString(StringID::ProgramName),
+          wxDefaultPosition, wxSize(500, 600))
 {
+#ifdef WIN32
+    SetIcons(wxICON(aaaaaaMAIN_ICON));
+#endif
+
     manager.SetManagedWindow(this);
     wxMenu* menuFile = new wxMenu;
     menuFile->Append(GUI_IDS::ID_Hello, "&Hello...\tCtrl-H",
@@ -29,12 +34,14 @@ MainFrame::MainFrame()
     Bind(wxEVT_MENU, &MainFrame::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &MainFrame::OnExit, this, wxID_EXIT);
 
-
     TreeWindow* tree   = new TreeWindow(this, -1);
     ViewWindow* viewer = new ViewWindow(this, -1);
-    manager.AddPane(tree, wxAuiPaneInfo().CenterPane().MinSize(tree->GetMinSize()));
-    manager.AddPane(viewer, wxAuiPaneInfo().Bottom().Caption(
-        localizedString(StringID::Viewer)).MinSize(viewer->GetMinSize()));
+    manager.AddPane(
+        tree, wxAuiPaneInfo().CenterPane().MinSize(tree->GetMinSize()));
+    manager.AddPane(viewer, wxAuiPaneInfo()
+                                .Bottom()
+                                .Caption(localizedString(StringID::Viewer))
+                                .MinSize(viewer->GetMinSize()));
     manager.Update();
     SetMinSize(wxSize(500, 600));
 }
